07fa42e2e49b19731cc6c9f58ac2d15a451918cd
Pouyan Heyratpour <me@pouyan.dev>
2023-12-21 05:20:17 +0330

    [bricks] add infrastructure error
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

--- bricks/wire.go
+++ bricks/wire.go
@@ -6,5 +6,6 @@ var (
 	ErrNotImplemented = errors.New("not implemented")
 	ErrInvalidInput   = errors.New("invalid input")
 	ErrNotFound       = errors.New("not found")
+	ErrInfrastructure = errors.New("infrastructure error")
 	ErrReachedEnd     = errors.New("reached end")
 )
