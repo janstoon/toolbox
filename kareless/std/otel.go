5f44e4bc3064131f36818f781e37c481cfdb1ad1
Pouyan Heyratpour <me@pouyan.dev>
2024-02-26 08:57:15 +0330

    [kareless] reformat
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

--- kareless/std/otel.go
+++ kareless/std/otel.go
@@ -17,7 +17,7 @@ import (
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
-	"go.opentelemetry.io/otel/semconv/v1.24.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.24.0"
 
 	"github.com/janstoon/toolbox/kareless"
 )
