10be76148d413a4803ea3d6f73288e6f8aadfae0
Pouyan Heyratpour <me@pouyan.dev>
2024-05-26 01:23:44 +0330

    chore(bricks)/errors: categorize
    add resolvable error for retry attempts
    
    Signed-off-by: Pouyan Heyratpour <me@pouyan.dev>

--- bricks/wire.go
+++ bricks/wire.go
@@ -3,15 +3,27 @@ package bricks
 import "errors"
 
 var (
-	ErrReachedEnd = errors.New("reached end")
+	ErrResolvable = errors.New("temporary issue")
 
-	ErrInvalidInput       = errors.New("invalid input")
-	ErrInvalidCredentials = errors.New("invalid credentials")
-	ErrPermissionDenied   = errors.New("permission denied")
-	ErrNotFound           = errors.New("not found")
+	// ----------------------
+	//     Customer-side
+	// ----------------------
 
-	ErrInfrastructure = errors.New("infrastructure error")
-	ErrNotImplemented = errors.New("not implemented")
+	ErrCustomerSide       = errors.New("customer-side error")
+	ErrLogical            = errors.Join(ErrCustomerSide, errors.New("logical issue"))
+	ErrInvalidInput       = errors.Join(ErrCustomerSide, errors.New("invalid input"))
+	ErrInvalidCredentials = errors.Join(ErrInvalidInput, errors.New("invalid credentials"))
+	ErrPermissionDenied   = errors.Join(ErrCustomerSide, errors.New("permission denied"))
+	ErrNotFound           = errors.Join(ErrCustomerSide, errors.New("not found"))
+	ErrReachedEnd         = errors.Join(ErrCustomerSide, errors.New("reached end"))
+
+	// ----------------------
+	//     Supplier-side
+	// ----------------------
+
+	ErrSupplierSide   = errors.New("supplier-side error")
+	ErrInfrastructure = errors.Join(ErrSupplierSide, ErrResolvable, errors.New("infrastructure"))
+	ErrNotImplemented = errors.Join(ErrSupplierSide, errors.New("not implemented"))
 )
 
 type MessageEnvelope struct {
